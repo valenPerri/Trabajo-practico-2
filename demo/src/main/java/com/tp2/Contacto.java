df080b065a939abea295e772c68ae99ba1d0b63d
valenPerri <valensalmon3@gmail.com>
2023-10-03 22:24:03 -0300

    contacto

--- demo/src/main/java/com/tp2/Contacto.java
+++ demo/src/main/java/com/tp2/Contacto.java
@@ -1,49 +1,46 @@
 package com.tp2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Contacto {
 
-  private String nombre= " ";
-  private String apellido= " ";
-  private String email;
+    private String nombre;
+    private String apellido;
+    private String email;
 
-  public String getNombre(){
-      return nombre;
-  }
-  public String getApellido(){
-      return apellido;
-  }
+    public Contacto(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        setEmail(email);
+    }
 
-  public String getEmail(){
-      return email;
-  }
+    public void setEmail(String email) {
+        String expresion = "^[A-Za-z0-9+_.-]+@(.+)$"; // define regex que da una expresion regular para verificar el  correo (letras, numeros seguidos de @)
+        Pattern pattern = Pattern.compile(expresion); // crea el objeto atraves de la expresion
+        Matcher matcheo = pattern.matcher(email); // machea el email con las expresiones que se cargaron por lo que si mail coincide hace el match sino lo toma como invalido
 
-  public void setNombre(String nombre){
-    this.nombre = nombre;
-}
+        if (matcheo.matches()) {
+            this.email = email; // aca hacemos el match
+        } else {
+            System.out.println("El correo electrónico no es válido");
+            this.email = null; // nulo si es inválido
+        }
+    }
 
-  public void setApellido(String apellido){
-    this.apellido = apellido;
-}
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email){
-    this.email = email;
-}
-
-
- public Contacto(String nombre,String apellido, String email){
-    this.nombre=nombre; 
-    this.apellido= apellido;
-    this.email=email;
-  }
-
-//validar correo aca fijarse como introducir 
-
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Correo electrónico: " + email);
+        System.out.println();
     }
 
 
 
 
 
-
-
-
+}
--- demo/src/test/java/BuzonTest.java
+++ demo/src/test/java/BuzonTest.java
@@ -1,3 +1,8 @@
+
+
 public class BuzonTest {
+   
     
+
+
 }
